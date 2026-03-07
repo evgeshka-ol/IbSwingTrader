efe554f043d778f5dd115df67f72d743ecdb6dfb
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-07 20:31:49 +0200

    implement the model

--- IbSwingTrader/Models/Candle.cs
+++ IbSwingTrader/Models/Candle.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace IbSwingTrader.Models
+﻿namespace IbSwingTrader.Models
 {
-    internal class Candle
+    public class Candle
     {
+        public Timeframe Timeframe { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public decimal Open { get; set; }
+
+        public decimal High { get; set; }
+
+        public decimal Low { get; set; }
+
+        public decimal Close { get; set; }
+
+        public long Volume { get; set; }
     }
 }
