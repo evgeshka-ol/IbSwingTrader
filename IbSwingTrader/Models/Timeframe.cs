9816e0b57399fc2d6088441ed6b452ab3e68d9ef
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-07 20:31:33 +0200

    add time frame enum

---
+++ IbSwingTrader/Models/Timeframe.cs
@@ -0,0 +1,14 @@
+﻿namespace IbSwingTrader.Models
+{
+    public enum Timeframe
+    {
+        M1,
+        M5,
+        M15,
+        M30,
+        H1,
+        H4,
+        D1,
+        W1
+    }
+}
