188b84192061ea2457c612fad22e7d8f45467823
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-07 21:04:04 +0200

    rename the model and implement it

---
+++ IbSwingTrader/Models/TradeRecord.cs
@@ -0,0 +1,17 @@
+﻿namespace IbSwingTrader.Models
+{
+    public class TradeRecord
+    {
+        public string Ticker { get; set; } = "";
+
+        public DateTime EntryTime { get; set; }
+        public decimal EntryPrice { get; set; }
+
+        public DateTime ExitTime { get; set; }
+        public decimal ExitPrice { get; set; }
+
+        public decimal ProfitPercent { get; set; }
+
+        public int HoldDays { get; set; }
+    }
+}
