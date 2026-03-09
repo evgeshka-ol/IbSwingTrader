a11f689f428a137ec8e4ec050134fbe9ba2d8744
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-09 12:26:10 +0200

    add date time extension

---
+++ IbSwingTrader/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace IbSwingTrader.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToIbEndTime(this DateTime utc)
+        {
+            if (utc.Kind != DateTimeKind.Utc)
+                utc = utc.ToUniversalTime();
+
+            return utc.ToString("yyyyMMdd HH:mm:ss 'UTC'");
+        }
+    }
+}
