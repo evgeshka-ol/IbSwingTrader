6c8d49e5369f514ab98b176869fc2bc8c4646203
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-09 12:28:07 +0200

    implement market data provider

--- IbSwingTrader/MarketData/IB/TwsMarketDataProvider.cs
+++ IbSwingTrader/MarketData/IB/TwsMarketDataProvider.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Concurrent;
+using IBApi;
+using IbSwingTrader.Extensions;
+using IbSwingTrader.MarketData.Interfaces;
+using IbSwingTrader.Models;
 
-namespace IbSwingTrader.Data.IB
+namespace IbSwingTrader.MarketData.IB
 {
-    internal class HistoricalDataService
+    public class TwsMarketDataProvider(EClientSocket client) : IMarketDataProvider
     {
+        private readonly EClientSocket _client = client;
+
+        private int _nextRequestId = 1;
+
+        private readonly ConcurrentDictionary<int, TaskCompletionSource<List<Candle>>> _requests
+            = new();
+
+        private readonly ConcurrentDictionary<int, List<Candle>> _buffers
+            = new();
+
+        public async Task<List<Candle>> GetCandles(
+            string ticker,
+            Timeframe timeframe,
+            DateTime endTimeUtc,
+            int bars)
+        {
+            var contract = TwsContractFactory.CreateStock(ticker);
+
+            var reqId = Interlocked.Increment(ref _nextRequestId);
+
+            var tcs = new TaskCompletionSource<List<Candle>>();
+
+            _requests[reqId] = tcs;
+            _buffers[reqId] = new List<Candle>();
+
+            _client.reqHistoricalData(
+                reqId,
+                contract,
+                endTimeUtc.ToIbEndTime(),
+                timeframe.ToIBDuration(bars),
+                timeframe.ToIBBarSize(),
+                "TRADES",
+                0,
+                1,
+                false,
+                null);
+
+            return await tcs.Task;
+        }
     }
 }
