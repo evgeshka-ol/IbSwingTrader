c11a3d7ce412c0578f4a5fbf1b30d674af9cb2cf
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-09 14:06:38 +0200

    add parser for ibkr string time

---
+++ IbSwingTrader/MarketData/IB/TwsTimeParser.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace IbSwingTrader.MarketData.IB
+{
+    public static class TwsTimeParser
+    {
+        public static DateTime ParseToUtc(string ibTime)
+        {
+            var parts = ibTime.Split(' ');
+
+            // DAILY BAR
+            if (parts.Length == 1)
+            {
+                var date = DateTime.ParseExact(
+                    parts[0],
+                    "yyyyMMdd",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.AssumeUniversal);
+
+                return date;
+            }
+
+            // INTRADAY BAR
+            var localTime = DateTime.ParseExact(
+                $"{parts[0]} {parts[1]}",
+                "yyyyMMdd HH:mm:ss",
+                CultureInfo.InvariantCulture);
+
+            var tz = ParseIbTimezone(parts[2]);
+
+            return TimeZoneInfo.ConvertTimeToUtc(localTime, tz);
+        }
+
+        private static TimeZoneInfo ParseIbTimezone(string tz)
+        {
+            return tz switch
+            {
+                "US/Eastern" => TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"),
+                "UTC" => TimeZoneInfo.Utc,
+                _ => TimeZoneInfo.Utc
+            };
+        }
+    }
+}
