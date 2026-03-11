4120ee83bed90416df6c1c32e0703867862dbff0
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-11 21:36:11 +0200

    inherit data set row model from the trade record model

--- IbSwingTrader/Models/TradeDatasetRow.cs
+++ IbSwingTrader/Models/TradeDatasetRow.cs
@@ -1,27 +1,11 @@
 ﻿namespace IbSwingTrader.Models
 {
-    public class TradeDatasetRow
+    public class TradeDatasetRow : TradeRecord
     {
-        // базовая информация
-        public required string Ticker { get; set; }
-
-        public DateTime EntryDate { get; set; }
-
-        public double EntryPrice { get; set; }
-
-        // реальная сделка или синтетическая
         public bool IsRealTrade { get; set; }
-
-        // данные сделки (если реальная)
-        public double? ExitPrice { get; set; }
-
-        public int? HoldDays { get; set; }
-
-        public double? ProfitPct { get; set; }
 
         // признаки рынка в момент входа
         public double Pullback5d { get; set; }
-
         public double Pullback10d { get; set; }
 
         public double BBPosition { get; set; }
@@ -36,18 +20,15 @@
 
         public double TrendPosition { get; set; }
 
-        // будущие движения
+        // будущее движение
         public double FutureHigh1d { get; set; }
-
         public double FutureHigh2d { get; set; }
 
         public double FutureLow1d { get; set; }
-
         public double FutureLow2d { get; set; }
 
-        // цели стратегии
+        // таргеты стратегии
         public bool Target10pct1d { get; set; }
-
         public bool Target10pct2d { get; set; }
     }
 }
