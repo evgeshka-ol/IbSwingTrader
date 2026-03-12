624b13b07a1abd4988d407d2b17a68c2c55a4061
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-12 13:12:29 +0200

    add daily and weekly context

--- IbSwingTrader/Models/TradeDatasetRow.cs
+++ IbSwingTrader/Models/TradeDatasetRow.cs
@@ -4,30 +4,32 @@
     {
         public bool IsRealTrade { get; set; }
 
-        // признаки рынка в момент входа
+        // --- 4H indicators ---
         public double Pullback5d { get; set; }
         public double Pullback10d { get; set; }
-
         public double BBPosition { get; set; }
-
         public double VolumeRatio20 { get; set; }
-
         public double RSI14 { get; set; }
-
         public double ATRRatio { get; set; }
-
         public double MACDHist { get; set; }
-
         public double TrendPosition { get; set; }
 
-        // будущее движение
+        // --- daily context ---
+        public double DailyTrendPosition { get; set; }
+        public double DailyPullback10d { get; set; }
+        public double DailyRSI14 { get; set; }
+
+        // --- weekly context ---
+        public double WeeklyTrendPosition { get; set; }
+        public bool WeeklyUptrend { get; set; }
+
+        // --- future movement ---
         public double FutureHigh1d { get; set; }
         public double FutureHigh2d { get; set; }
-
         public double FutureLow1d { get; set; }
         public double FutureLow2d { get; set; }
 
-        // таргеты стратегии
+        // --- targets ---
         public bool Target10pct1d { get; set; }
         public bool Target10pct2d { get; set; }
     }
