a165e46f85890476877d3281fdc6483e6ff6f625
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-13 11:46:04 +0200

    add interface for the dataset builder

---
+++ IbSwingTrader/MarketData/Interfaces/ITradeDatasetBuilder.cs
@@ -0,0 +1,9 @@
+﻿using IbSwingTrader.Models;
+
+namespace IbSwingTrader.MarketData.Interfaces
+{
+    public interface ITradeDatasetBuilder
+    {
+        List<TradeDatasetRow> Build(List<TradeRecord> trades, List<Candle> candles);
+    }
+}
