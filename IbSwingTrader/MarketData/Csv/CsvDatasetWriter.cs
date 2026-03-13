c6481133a6e0107fef5f58151dfbc33ffa33f92b
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-13 11:49:56 +0200

    implement CSV writer

---
+++ IbSwingTrader/MarketData/Csv/CsvDatasetWriter.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using IbSwingTrader.MarketData.Interfaces;
+
+namespace IbSwingTrader.MarketData.Csv
+{
+    public class CsvDatasetWriter : ICsvWriter
+    {
+        public void Write<T>(string path, IEnumerable<T> rows)
+        {
+            using var writer = new StreamWriter(path);
+
+            var props = typeof(T).GetProperties();
+
+            writer.WriteLine(string.Join(",", props.Select(p => p.Name)));
+
+            foreach (var row in rows)
+            {
+                var values = props.Select(p =>
+                {
+                    var value = p.GetValue(row);
+
+                    if (value == null)
+                        return "";
+
+                    if (value is decimal d)
+                        return d.ToString("F6", CultureInfo.InvariantCulture);
+
+                    if (value is DateTime dt)
+                        return dt.ToString("yyyy-MM-dd HH:mm:ss");
+
+                    return value.ToString();
+                });
+
+                writer.WriteLine(string.Join(",", values));
+            }
+        }
+    }
+}
