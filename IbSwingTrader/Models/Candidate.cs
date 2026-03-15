c910e43025161df72f368afc2c7789d419e9ae01
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-15 20:39:22 +0200

    add models for the candidate

---
+++ IbSwingTrader/Models/Candidate.cs
@@ -0,0 +1,19 @@
+﻿namespace IbSwingTrader.Models
+{
+    public class Candidate
+    {
+        public required string Ticker { get; set; }
+
+        public decimal EntryPrice { get; set; }
+
+        public decimal ExitPrice { get; set; }
+
+        public decimal StopLoss { get; set; }
+
+        public decimal ProfitPercent { get; set; }
+
+        public decimal LossPercent { get; set; }
+
+        public decimal Score { get; set; }
+    }
+}
---
+++ IbSwingTrader/Models/CandidateDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace IbSwingTrader.Models
+{
+    public class CandidateDetails : Candidate
+    {
+        public decimal Pullback10d { get; set; }
+
+        public decimal DistanceTo20dHigh { get; set; }
+
+        public decimal DistanceTo52wHigh { get; set; }
+
+        public decimal VolumeRatio20 { get; set; }
+
+        public decimal ATRRatio { get; set; }
+
+        public decimal TrendPosition { get; set; }
+    }
+}
