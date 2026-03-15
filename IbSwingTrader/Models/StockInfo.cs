95736ba2f90132d58767b7811e3daeea61912fa8
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-15 20:11:37 +0200

    add stock prefilter and a model for it

---
+++ IbSwingTrader/Models/StockInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace IbSwingTrader.Models
+{
+    public class StockInfo
+    {
+        public required string Ticker { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal MarketCap { get; set; }
+
+        public long AvgVolume20 { get; set; }
+    }
+}
---
+++ IbSwingTrader/Services/CandidateFiltering/StockPreFilter.cs
@@ -0,0 +1,36 @@
+﻿using IbSwingTrader.Interfaces;
+using IbSwingTrader.Models;
+
+namespace IbSwingTrader.Services.CandidateFiltering
+{
+    public class StockPreFilter : IStockPreFilter
+    {
+        public decimal MinPrice { get; set; } = 5m;
+        public decimal MaxPrice { get; set; } = 200m;
+
+        public decimal MinMarketCap { get; set; } = 1_000_000_000m;
+
+        public long MinAvgVolume { get; set; } = 1_000_000;
+
+        public decimal MinDollarVolume { get; set; } = 10_000_000m;
+
+        public bool Pass(StockInfo stock)
+        {
+            if (stock.Price < MinPrice || stock.Price > MaxPrice)
+                return false;
+
+            if (stock.MarketCap < MinMarketCap)
+                return false;
+
+            if (stock.AvgVolume20 < MinAvgVolume)
+                return false;
+
+            var dollarVolume = stock.Price * stock.AvgVolume20;
+
+            if (dollarVolume < MinDollarVolume)
+                return false;
+
+            return true;
+        }
+    }
+}
