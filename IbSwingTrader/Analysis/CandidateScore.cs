3bcc05cc34dc34e568cc93ce655004e786c733f6
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-15 21:37:11 +0200

    add the class for calculating candidate score

---
+++ IbSwingTrader/Analysis/CandidateScore.cs
@@ -0,0 +1,20 @@
+﻿using IbSwingTrader.Interfaces;
+using IbSwingTrader.Models;
+
+namespace IbSwingTrader.Analysis
+{
+    public class CandidateScore : ICandidateScore
+    {
+        public decimal Calculate(FeatureSet f)
+        {
+            decimal score =
+                (-f.DistanceTo20dHigh * 0.35m)
+                + (-f.Pullback10d * 0.25m)
+                + (f.VolumeRatio20 * 0.15m)
+                + ((0.08m - f.ATRRatio) * 100 * 0.15m)
+                + ((1 - f.TrendPosition) * 100 * 0.1m);
+
+            return score;
+        }
+    }
+}
---
+++ IbSwingTrader/Interfaces/ICandidateScore.cs
@@ -0,0 +1,9 @@
+﻿using IbSwingTrader.Models;
+
+namespace IbSwingTrader.Interfaces
+{
+    public interface ICandidateScore
+    {
+        decimal Calculate(FeatureSet f);
+    }
+}
