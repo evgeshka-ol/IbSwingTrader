b9248318dac40e6edf8056e5577ac6b8e31a4951
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-16 14:57:19 +0200

    implement trade builder

---
+++ IbSwingTrader/Interfaces/ITradeBuilder.cs
@@ -0,0 +1,9 @@
+﻿using IbSwingTrader.Models;
+
+namespace IbSwingTrader.Interfaces
+{
+    public interface ITradeBuilder
+    {
+        TradePlan Build(List<Candle> candles);
+    }
+}
---
+++ IbSwingTrader/Services/TradeBuilder.cs
@@ -0,0 +1,26 @@
+﻿using IbSwingTrader.Interfaces;
+using IbSwingTrader.Models;
+
+namespace IbSwingTrader.Services
+{
+    public class TradeBuilder : ITradeBuilder
+    {
+        public TradePlan Build(List<Candle> candles)
+        {
+            var last = candles[^1];
+
+            var entry = last.Close;
+
+            var stop = entry * 0.95m;
+
+            var exit = entry * 1.15m;
+
+            return new TradePlan
+            {
+                Entry = entry,
+                Exit = exit,
+                Stop = stop
+            };
+        }
+    }
+}
