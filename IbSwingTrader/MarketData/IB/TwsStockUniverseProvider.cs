f481126d0b5a11ccd7307cb2dc37c7b49d016433
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-16 18:50:04 +0200

    change scanner parameters

--- IbSwingTrader/MarketData/IB/TwsStockUniverseProvider.cs
+++ IbSwingTrader/MarketData/IB/TwsStockUniverseProvider.cs
@@ -19,7 +19,8 @@ namespace IbSwingTrader.MarketData.IB
                 Instrument = "STK",
                 LocationCode = _settings.LocationCode,
                 ScanCode = _settings.ScanCode,
-                NumberOfRows = 50
+                NumberOfRows = 50,
+                AbovePrice = 1
             };
 
             var filters = new List<TagValue>();
