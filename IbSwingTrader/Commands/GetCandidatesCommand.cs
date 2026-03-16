1111c05f55c048d77dd0ea39803b8b61e85b29d7
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-16 15:48:58 +0200

    implement command for getting candidates

---
+++ IbSwingTrader/Commands/GetCandidatesCommand.cs
@@ -0,0 +1,19 @@
+﻿using IbSwingTrader.Interfaces;
+using IbSwingTrader.Services;
+
+namespace IbSwingTrader.Commands
+{
+    public class GetCandidatesCommand(
+        CandidateFinder finder,
+        ICandidateResultWriter writer)
+    {
+        private readonly CandidateFinder _finder = finder;
+        private readonly ICandidateResultWriter _writer = writer;
+
+        public async Task RunAsync()
+        {
+            var candidates = await _finder.FindAsync();
+            await _writer.WriteAsync(candidates);
+        }
+    }
+}
