07cbdb3512bdee2fa2e4b902b2d6dfae460b61db
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-16 14:58:24 +0200

    implement candidate filter

---
+++ IbSwingTrader/Services/CandidateFiltering/CandidateFilter.cs
@@ -0,0 +1,22 @@
+﻿using IbSwingTrader.Interfaces;
+using IbSwingTrader.Models;
+
+namespace IbSwingTrader.Services.CandidateFiltering
+{
+    public class CandidateFilter : ICandidateFilter
+    {
+        public bool Pass(FeatureSet f)
+        {
+            if (f.VolumeRatio20 < 1.2m)
+                return false;
+
+            if (f.DistanceTo20dHigh < -15m)
+                return false;
+
+            if (f.ATRRatio > 0.12m)
+                return false;
+
+            return true;
+        }
+    }
+}
