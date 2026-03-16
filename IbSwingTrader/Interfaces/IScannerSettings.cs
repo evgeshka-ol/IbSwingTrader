bbc465cc9b23ac6eb0b8f5c4cf209013ca82e332
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-16 14:58:50 +0200

    add scanner settings

---
+++ IbSwingTrader/Interfaces/IScannerSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace IbSwingTrader.Interfaces
+{
+    public interface IScannerSettings
+    {
+        string LocationCode { get; }
+        string ScanCode { get; }
+
+        decimal MinPrice { get; }
+        decimal MaxPrice { get; }
+
+        decimal MinMarketCap { get; }
+        long MinAvgVolume { get; }
+
+        decimal MinDollarVolume { get; }
+    }
+}
---
+++ IbSwingTrader/Services/CandidateFiltering/ScannerSettings .cs
@@ -0,0 +1,20 @@
+﻿using IbSwingTrader.Interfaces;
+
+namespace IbSwingTrader.Services.CandidateFiltering
+{
+    public class ScannerSettings : IScannerSettings
+    {
+        public string LocationCode { get; set; } = "STK.US.MAJOR";
+
+        public string ScanCode { get; set; } = "MOST_ACTIVE";
+
+        public decimal MinPrice { get; set; } = 5m;
+        public decimal MaxPrice { get; set; } = 200m;
+
+        public decimal MinMarketCap { get; set; } = 1_000_000_000m;
+
+        public long MinAvgVolume { get; set; } = 1_000_000;
+
+        public decimal MinDollarVolume { get; set; } = 10_000_000m;
+    }
+}
