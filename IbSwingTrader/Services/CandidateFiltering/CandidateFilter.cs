edc3a47e94fa3c33bb80e6ea1393677cd3e8e074
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-18 16:37:41 +0200

    improve candidate filter

--- IbSwingTrader/Services/CandidateFiltering/CandidateFilter.cs
+++ IbSwingTrader/Services/CandidateFiltering/CandidateFilter.cs
@@ -9,7 +9,7 @@ namespace IbSwingTrader.Services.CandidateFiltering
 
         public decimal MinPrice { get; set; } = 5m;
         public decimal MaxPrice { get; set; } = 200m;
-        public decimal MinDollarVolume { get; set; } = 10_000_000m;
+        public decimal MinDollarVolume { get; set; } = 5_000_000m;
 
         public bool Pass(
             FeatureSet f,
@@ -36,52 +36,21 @@ namespace IbSwingTrader.Services.CandidateFiltering
                 return false;
             }
 
-            // Хотим бумагу ниже средней, а не уже убежавшую вверх
-            if (f.BBMidSignedDistancePct > -1m)
+            if (f.BBMidSignedDistancePct > 0.5m)
             {
-                _logger.Info($"Filtered out due to BBMidSignedDistancePct {f.BBMidSignedDistancePct} above maximum -1");
+                _logger.Info($"Filtered out due to BBMidSignedDistancePct {f.BBMidSignedDistancePct} above 0.5");
                 return false;
             }
 
-            // Хотим не near-high momentum, а более глубокий откат
-            if (f.DistanceTo20dHigh > -40m)
+            if (f.DistanceTo20dHigh > -20m)
             {
-                _logger.Info($"Filtered out due to DistanceTo20dHigh {f.DistanceTo20dHigh} above maximum -40");
+                _logger.Info($"Filtered out due to DistanceTo20dHigh {f.DistanceTo20dHigh} above -20");
                 return false;
             }
 
-            // Дневной контекст не должен быть перегрет
-            if (f.DailyTrendPosition > 0.70m)
-            {
-                _logger.Info($"Filtered out due to DailyTrendPosition {f.DailyTrendPosition} above maximum 0.70");
-                return false;
-            }
-
-            // Нужен вменяемый дневной pullback, а не хаос и не отсутствие отката
-            if (f.DailyPullback10d < -50m || f.DailyPullback10d > -15m)
-            {
-                _logger.Info($"Filtered out due to DailyPullback10d {f.DailyPullback10d} outside range [-50; -15]");
-                return false;
-            }
-
-            // Слишком большой volume spike не обязателен, часто даже мешает
-            if (f.VolumeRatio20 > 1.0m)
-            {
-                _logger.Info($"Filtered out due to VolumeRatio20 {f.VolumeRatio20} above maximum 1.0");
-                return false;
-            }
-
-            // Weekly momentum: лучше слабонегативный/переходный, а не already extended
-            if (f.WeeklyMACDHistDelta < -1.0m || f.WeeklyMACDHistDelta > -0.05m)
-            {
-                _logger.Info($"Filtered out due to WeeklyMACDHistDelta {f.WeeklyMACDHistDelta} outside range [-1.0; -0.05]");
-                return false;
-            }
-
-            // Ограничение по ATR можно оставить как защитное
             if (f.ATRRatio > 0.20m)
             {
-                _logger.Info($"Filtered out due to ATRRatio {f.ATRRatio} above maximum 0.20");
+                _logger.Info($"Filtered out due to ATRRatio {f.ATRRatio} above 0.20");
                 return false;
             }
 
