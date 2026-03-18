8c33cc753719a2fef9a419eb0a86302bcbaec2c8
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-18 11:01:52 +0200

    make candidate result writer more generic

--- IbSwingTrader/Infrastructure/Logging/CandidateResultWriter.cs
+++ IbSwingTrader/Infrastructure/Logging/CandidateResultWriter.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.Json;
+﻿using System.Globalization;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using IbSwingTrader.Interfaces;
 using IbSwingTrader.Models;
 
@@ -6,19 +9,89 @@ namespace IbSwingTrader.Infrastructure.Logging
 {
     public class CandidateResultWriter() : ICandidateResultWriter
     {
-        private readonly string _file = $"candidates_{DateTime.UtcNow:yyyyMMdd_HHmm}.json";
-        private static readonly JsonSerializerOptions _jsonOptions = new() { WriteIndented = true };
+        private readonly string _file = $"candidates/candidates_{DateTime.UtcNow:yyyyMMdd_HHmm}.json";
 
         public async Task WriteAsync(List<CandidateDetails> candidates)
         {
+            Directory.CreateDirectory(Path.GetDirectoryName(_file)!);
+
             foreach (var c in candidates)
             {
                 WriteCandidate(c);
             }
 
-            var json = JsonSerializer.Serialize(candidates, _jsonOptions);
+            var json = BuildJson(candidates);
 
             await File.WriteAllTextAsync(_file, json);
+        }
+
+        private static string BuildJson(IEnumerable<CandidateDetails> candidates)
+        {
+            var array = new JsonArray();
+
+            foreach (var candidate in candidates)
+            {
+                array.Add(ToJsonObject(candidate));
+            }
+
+            return array.ToJsonString(new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+        }
+
+        private static JsonObject ToJsonObject<T>(T item)
+        {
+            var type = typeof(T);
+
+            var baseProps = type.BaseType?
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .OrderBy(p => p.MetadataToken)
+                ?? Enumerable.Empty<PropertyInfo>();
+
+            var ownProps = type
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                .OrderBy(p => p.MetadataToken);
+
+            var props = baseProps
+                .Concat(ownProps)
+                .ToArray();
+
+            var obj = new JsonObject();
+
+            foreach (var prop in props)
+            {
+                var value = prop.GetValue(item);
+                obj[prop.Name] = ToJsonNode(value, prop.Name);
+            }
+
+            return obj;
+        }
+
+        private static JsonNode? ToJsonNode(object? value, string propertyName)
+        {
+            if (value == null)
+                return null;
+
+            return value switch
+            {
+                decimal d => JsonValue.Create(decimal.Parse(
+                    d.ToString("F6", CultureInfo.InvariantCulture),
+                    CultureInfo.InvariantCulture)),
+
+                DateTime dt => JsonValue.Create(
+                    propertyName.EndsWith("Date", StringComparison.OrdinalIgnoreCase)
+                        ? dt.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)
+                        : dt.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)),
+
+                bool b => JsonValue.Create(b),
+                int i => JsonValue.Create(i),
+                long l => JsonValue.Create(l),
+                double d => JsonValue.Create(d),
+                float f => JsonValue.Create(f),
+
+                _ => JsonValue.Create(value.ToString())
+            };
         }
 
         private static void WriteCandidate(Candidate c)
