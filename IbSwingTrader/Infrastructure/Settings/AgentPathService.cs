54559eb61117808621ac59e88f9f20faa9542227
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-21 18:55:49 +0200

    add the path service

---
+++ IbSwingTrader/Infrastructure/Settings/AgentPathService.cs
@@ -0,0 +1,27 @@
+﻿using IbSwingTrader.Interfaces;
+using IbSwingTrader.Models;
+
+namespace IbSwingTrader.Infrastructure.Settings
+{
+    public class AgentPathService : IAgentPathService
+    {
+        private readonly PathSettings _paths;
+
+        public AgentPathService(IAgentSettingsProvider settingsProvider)
+        {
+            _paths = settingsProvider.Get().Paths;
+        }
+
+        public string GetDataRoot() => _paths.DataRoot;
+        public string GetCacheFolder() => Combine(_paths.CacheFolder);
+        public string GetDatasetFolder() => Combine(_paths.DatasetFolder);
+        public string GetCandidatesFolder() => Combine(_paths.CandidatesFolder);
+        public string GetEvaluationsFolder() => Combine(_paths.EvaluationsFolder);
+        public string GetWishListFile() => Combine(_paths.WishListFile);
+        public string GetProcessedCandidateFilesManifest() => Combine(_paths.ProcessedCandidateFilesManifest);
+        public string GetLogsFolder() => Combine(_paths.LogsFolder);
+
+        private string Combine(string relativePath) =>
+            Path.Combine(_paths.DataRoot, relativePath);
+    }
+}
---
+++ IbSwingTrader/Interfaces/IAgentPathService.cs
@@ -0,0 +1,14 @@
+﻿namespace IbSwingTrader.Interfaces
+{
+    public interface IAgentPathService
+    {
+        string GetDataRoot();
+        string GetCacheFolder();
+        string GetDatasetFolder();
+        string GetCandidatesFolder();
+        string GetEvaluationsFolder();
+        string GetWishListFile();
+        string GetProcessedCandidateFilesManifest();
+        string GetLogsFolder();
+    }
+}
