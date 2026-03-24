00d2ddc4faec201d846558903c77d9ce407cb7a2
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-24 12:26:43 +0200

    add the score info model

---
+++ IbSwingTrader/Models/Tickers/ScoreInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace IbSwingTrader.Models.Tickers
+{
+    public class ScoreInfo
+    {
+        public decimal Score { get; set; }
+
+        public decimal? WeeklyScore { get; set; }
+
+        public decimal? DailyScore { get; set; }
+
+        public decimal? EntryScore { get; set; }
+    }
+}
