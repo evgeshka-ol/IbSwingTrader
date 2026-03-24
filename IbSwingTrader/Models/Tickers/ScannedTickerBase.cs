0198a76370327e1607977bd6a7ac811dc78b96a0
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-24 11:55:09 +0200

    add the scanned ticker base model

---
+++ IbSwingTrader/Models/Tickers/ScannedTickerBase.cs
@@ -0,0 +1,29 @@
+﻿namespace IbSwingTrader.Models.Tickers
+{
+    public abstract class ScannedTickerBase : TickerEntity
+    {
+        public string PresetScanCode { get; set; } = string.Empty;
+
+        public string PresetDescription { get; set; } = string.Empty;
+
+        public DateTime ScanTimeMarket { get; set; }
+
+        public string ScanTimeZone { get; set; } = string.Empty;
+
+        public decimal Score { get; set; }
+
+        public decimal? WeeklyScore { get; set; }
+
+        public decimal? DailyScore { get; set; }
+
+        public decimal? EntryScore { get; set; }
+
+        public decimal DistanceTo20dHigh { get; set; }
+
+        public decimal DistanceTo52wHigh { get; set; }
+
+        public decimal DailyRSI14 { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
