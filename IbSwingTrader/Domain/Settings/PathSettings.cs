297b63a21e05abac5611792723905dd6e477b2a2
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-03-30 15:10:50 +0300

    move existing dataset to a new file in the datasets folder

--- IbSwingTrader/Domain/Settings/PathSettings.cs
+++ IbSwingTrader/Domain/Settings/PathSettings.cs
@@ -4,7 +4,7 @@ namespace IbSwingTrader.Domain.Settings
     {
         public string DataRoot { get; set; } = "Data";
         public string TradesFile { get; set; } = "trades.csv";
-        public string DatasetFile { get; set; } = "dataset.csv";
+        public string DatasetFile { get; set; } = "datasets/trade_dataset.csv";
         public string CacheFolder { get; set; } = "cache";
         public string CandidatesFile { get; set; } = "Tickers/candidates.json";
         public string EvaluationsFile { get; set; } = "Tickers/evaluations.csv";
