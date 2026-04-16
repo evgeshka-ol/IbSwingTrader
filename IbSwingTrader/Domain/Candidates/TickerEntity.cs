3d22d8998edc4363210d0145a5857bc68b9b9778
Yevhen Olieniev <y.olieniev@i-sec.com>
2026-04-16 13:43:20 +0300

    fix properties order

--- IbSwingTrader/Domain/Candidates/TickerEntity.cs
+++ IbSwingTrader/Domain/Candidates/TickerEntity.cs
@@ -2,6 +2,7 @@ namespace IbSwingTrader.Domain.Candidates
 {
     public abstract class TickerEntity
     {
+        [System.Text.Json.Serialization.JsonPropertyOrder(-1000)]
         public required string Ticker { get; set; }
     }
 }
